d39ea81151fbea7072ef98e9e523cac25a679fba
rlegendi <richard.legendi@gmail.com>
2017-07-31 21:37:30 +0200

    New features: community detection and in-degree based vertex size

--- sociometry.R
+++ sociometry.R
@@ -23,6 +23,10 @@ SIMPLIFY <- TRUE
 CHART_WIDTH <- 800
 CHART_HEIGHT <- 600
 
+# Used for community detection, denotes the number of communities to taxify.
+# See https://arxiv.org/abs/physics/0512106 for the details.
+RANDOMWALK_COMMUNITY_NUMBER <- 4
+
 # Determines if messages should be logged in the output
 MESSAGES <- TRUE
 
@@ -54,7 +58,8 @@ readCsv <- function(csvName) {
 }
 
 ### Graphing methods
-plotGraph <- function(relations, title, showLabels) {
+# layoutHandling - default is idontcare, creates a new layout for each graph
+plotGraph <- function(relations, title, showLabels, layoutHandling=c("idontcare", "store", "forget")) {
 	dir.create(OUTPUT_BASE_DIR, showWarnings = FALSE)
 	
 	g <- graph.data.frame(d=relations, directed=T)
@@ -64,25 +69,35 @@ plotGraph <- function(relations, title, showLabels) {
 		g <- simplify(g, remove.multiple = T, remove.loops = F)
 	}
 	
+	if (layoutHandling == "store") {
+		act.layout <<- layout.fruchterman.reingold(g)
+	}
+	
 	if (!showLabels) {
 		title <- paste(title, "Anon")
 		V(g)$label <- NA
 	}
 	
+	membership <- cut_at(walktrap.community(g), no=RANDOMWALK_COMMUNITY_NUMBER)
+	
 	fileName <- paste(title, ifelse(showLabels, "Labels", "NoLabels"))
 	
 	png(paste(OUTPUT_BASE_DIR, "\\", fileName, ".png", sep=""), width=CHART_WIDTH, height=CHART_HEIGHT)
-	plot(g, main=title)
+	plot(g, main=title, layout=act.layout, vertex.color= rainbow(10, .8, .8, alpha=.8)[membership], vertex.size=degree(g, mode="in")+8)
 	dev.off()
+	
+	if (layoutHandling == "forget") {
+		act.layout <<- NULL
+	}
 }
 
-plotLeads <- function(csv, showLabels) {
+plotLeads <- function(csv, showLabels, layoutHandling=c("store", "forget")) {
 	rel <- data.frame(from = csv[, "Your.name."], to = csv[,"Who.would.you.choose.to.lead.you."])
 	
-	plotGraph(rel, "Leads", showLabels)
+	plotGraph(rel, "Leads", showLabels, layoutHandling=layoutHandling)
 }
 
-plotWork <- function(csv, showLabels) {
+plotWork <- function(csv, showLabels, layoutHandling=c("store", "forget")) {
 	rel <- data.frame(
 			from = c(rep(csv[,"Your.name."], 4)),
 			to = c(
@@ -92,10 +107,10 @@ plotWork <- function(csv, showLabels) {
 					csv[, "Name.3"]
 			))
 	
-	plotGraph(rel, "Work", showLabels)
+	plotGraph(rel, "Work", showLabels, layoutHandling=layoutHandling)
 }
 
-plotFun <- function(csv, showLabels) {
+plotFun <- function(csv, showLabels, layoutHandling=c("store", "forget")) {
 	rel <- data.frame(
 			from = c(rep(csv[,"Your.name."], 3)),
 			to = c(
@@ -104,10 +119,10 @@ plotFun <- function(csv, showLabels) {
 					csv[, "Name.32"]
 			))
 	
-	plotGraph(rel, "Fun", showLabels)
+	plotGraph(rel, "Fun", showLabels, layoutHandling=layoutHandling)
 }
 
-plotAll <- function(csv, showLabels) {
+plotAll <- function(csv, showLabels, layoutHandling=c("store", "forget")) {
 	rel <- data.frame(
 			from = c(rep(csv[,"Your.name."], 7)),
 			to = c(
@@ -120,19 +135,24 @@ plotAll <- function(csv, showLabels) {
 					csv[, "Name.32"]
 			))
 	
-	plotGraph(rel, "All", showLabels)
+	plotGraph(rel, "All", showLabels, layoutHandling=layoutHandling)
 }
 
 ### Processing
 
 csv <- readCsv("Sociometry - 2017H1.csv")
 
-for (showLabels in c(TRUE, FALSE)) {
-	plotLeads(csv, showLabels)
-	plotWork(csv, showLabels)
-	plotFun(csv, showLabels)
-	plotAll(csv, showLabels)
-}
+plotLeads(csv, TRUE, "store")
+plotLeads(csv, FALSE, "forget")
+
+plotWork(csv, TRUE, "store")
+plotWork(csv, FALSE, "forget")
+
+plotFun(csv, TRUE, "store")
+plotFun(csv, FALSE, "forget")
+
+plotAll(csv, TRUE, "store")
+plotAll(csv, FALSE, "forget")
 
 alarm()
 
